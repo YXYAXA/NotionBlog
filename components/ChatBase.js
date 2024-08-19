a4c29c5e7358537341058f34e771f01784f647d8
YuY <281625263@qq.com>
2024-08-19 14:56:56 +0800

    Update ChatBase.js

--- components/ChatBase.js
+++ components/ChatBase.js
@@ -1,33 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { siteConfig } from '@/lib/config'
 
+/**
+ * 这是一个嵌入组件，可以在任意位置全屏显示您的chat-base对话框
+ * 暂时没有页面引用
+ * 因为您可以直接用内嵌网页的方式放入您的notion中 https://www.chatbase.co/chatbot-iframe/${siteConfig('CHATBASE_ID')}
+ */
 export default function ChatBase() {
-  const clientRef = useRef(null);
+  if (!siteConfig('CHATBASE_ID')) {
+    return <></>
+  }
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/0.1.0-beta.5/libs/cn/index.js';
-    script.async = true;
-    script.onload = () => {
-      if (window.CozeWebSDK) {
-        clientRef.current = window.CozeWebSDK.WebChatClient({
-          config: {
-            bot_id:'7404668511764611111',
-          },
-          componentProps: {
-            title: 'Coze',
-          },
-        });
-      }
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-      if (clientRef.current && clientRef.current.destroy) {
-        clientRef.current.destroy();
-      }
-    };
-  }, []);
-
-  return null;
+  return <iframe
+        src={`https://www.chatbase.co/chatbot-iframe/${siteConfig('CHATBASE_ID')}`}
+        width="100%"
+        style={{ height: '100%', minHeight: '700px' }}
+        frameborder="0"
+    ></iframe>
 }
