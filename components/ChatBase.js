c795a315e6f539861f1374440b9b9a7a955b5fdc
YuY <281625263@qq.com>
2024-08-20 12:26:14 +0800

    Update ChatBase.js

--- components/ChatBase.js
+++ components/ChatBase.js
@@ -1,15 +1,9 @@
-import { siteConfig } from '@/lib/config'
-
 /**
  * 这是一个嵌入组件，可以在任意位置全屏显示您的chat-base对话框
  * 暂时没有页面引用
  * 因为您可以直接用内嵌网页的方式放入您的notion中 https://www.chatbase.co/chatbot-iframe/${siteConfig('CHATBASE_ID')}
  */
 export default function ChatBase() {
-  if (!siteConfig('CHATBASE_ID')) {
-    return <></>
-  }
-
   return <iframe
         src="https://share.fastgpt.in/chat/share?shareId=5g292sg65ng86xg4yneld3s6"
         width="100%"
